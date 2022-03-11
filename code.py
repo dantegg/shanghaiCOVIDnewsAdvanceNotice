eb5f7160eb83f5d77b3aebd7460ff1125be7f6eb
Zhang <dantegg327@gmail.com>
2022-03-11 09:08:16 +0800

    Add files via upload
    监控有没有最新的上海疫情发布会，然后windows弹窗通知

--- code.py
+++ code.py
@@ -1,30 +1,80 @@
+# from nis import match
 import time
 from win10toast import ToastNotifier
 import requests
+import datetime
+import re
+import time
 
-# r = requests.get("https://live.kankanews.com/")
-r = requests.get("https://api-app.kankanews.com/kankan/pc/live?nonce=ns4dt2vx&platform=pc&timestamp=1646897946&version=1.0&sign=212c27cf00c56419d95bdf0e044d8251")
-# html=r.text.find('class="u2"')
-# r.encoding= "utf-8"
-contentText = r.json()
-# html_doc=str(html,'gb2312') #html_doc=html.decode("utf-8","ignore")
-# print(html)
-print(contentText['result']['live_notice_list'])
-advanceList = contentText['result']['live_notice_list']
-for item in advanceList:
-    print('title is====' + item['title'])
-    print('intro is====' + item['intro'])
+recordCheck = True
+recordDate = datetime.date.today()
+todayReportCheck = False
 
-# print("==========response" + r.content.decode())
+def getNotice(nowDate):
+    global recordDate
+    global todayReportCheck
+    if nowDate == recordDate:
+        print("====today check")
+        if todayReportCheck is False:
+            print("===reporting===")
+            result = True
+            r = requests.get("https://api-app.kankanews.com/kankan/pc/live?nonce=ns4dt2vx&platform=pc&timestamp=1646897946&version=1.0&sign=212c27cf00c56419d95bdf0e044d8251")
+            contentText = r.json()
+            advanceList = contentText['result']['live_notice_list']
+            # advanceList = [{
+            #     "title": '123',
+            #     "intro": '邬惊雷'
+            # }]
+            todayMonth = datetime.date.today().month
+            todayDay = datetime.date.today().day
+            for item in advanceList:
+            # print('title is====' + item['title'])
+            # print('intro is====' + item['intro'])
+                title = item['title']
+                intro = item['intro']
+                matchStr = re.search('邬惊雷', intro)
+            # result = matchStr.search(intro)
+            # print( matchStr)
+                if matchStr is not None:
+                # print(title)
+                # print(intro)
+                # print(matchStr.group())
+                    toaster = ToastNotifier()
+                    toaster.show_toast(title,
+                    intro,
+                    icon_path = "test.ico",
+                    duration=20)
+                    print("advance notice get!")
+                    result = False
+                    todayReportCheck = True
+                    print("RECORD_CHECK!!!")
+                    print('TODAY_END!!!')
+                else:
+                    result = True
+            if result == True:
+                print("NOT_FOND")
+            return result
+        else:
+            return True
+    else:
+        recordDate = datetime.date.today()
+        todayReportCheck = False
+        print("=== new Day reset ===")
+        return True
 
-# http://www.shio.gov.cn/sh/xwb/n790/n791/index.html
+    
 
-# toaster = ToastNotifier()
-# toaster.show_toast("Hello World!!!",
-# "Python is 10 seconds",
-# icon_path = "test.ico",
-# duration=10)
+def sleepTime(hour, minute, second):
+    return hour * 3600 + minute * 60 + second
 
-# while toaster.notification_active():
-#     time.sleep(0.1)
-# print("hello")
+tenMinutes = sleepTime(0,5,0)
+
+while recordCheck:
+    time.sleep(tenMinutes)
+    nowTime = datetime.date.today()
+    if getNotice(nowTime) is False:
+        print('QUERY HAPPEN!!!')
+        continue
+print("end")
+
+
